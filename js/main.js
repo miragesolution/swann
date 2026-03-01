0576fd15d1b14acb2eec6e8db9af9a0d945b0ac1
Saiju Joseph <josephksaiju@gmail.com>
2026-03-01 17:33:47 +0530

    home page design partially completed

--- js/main.js
+++ js/main.js
@@ -50,6 +50,60 @@ document.addEventListener('DOMContentLoaded', () => {
    }
 });
 
+// owl carousel
+
+$(document).ready(function () {
+   $('.product-slider').owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: true,
+      dots: false,
+      navText: [
+         "<span class='custom-prev'>&#10094;</span>",
+         "<span class='custom-next'>&#10095;</span>"
+      ],
+      responsive: {
+         0: {
+            items: 1
+         },
+         576: {
+            items: 2
+         },
+         768: {
+            items: 3
+         },
+         1200: {
+            items: 4
+         }
+      }
+   });
+});
+
+// timer
+
+const targetDate = new Date('March 10, 2026 23:59:59').getTime();
+
+const countdown = setInterval(function () {
+   const now = new Date().getTime();
+   const distance = targetDate - now;
+
+   if (distance < 0) {
+      clearInterval(countdown);
+      document.getElementById('countdown').innerHTML = 'EXPIRED';
+      return;
+   }
+
+   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+   );
+   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+   document.getElementById('days').innerText = days;
+   document.getElementById('hours').innerText = hours;
+   document.getElementById('minutes').innerText = minutes;
+}, 1000);
+
 // billing details
 
 const checkbox = document.getElementById('createAccountCheckbox');
